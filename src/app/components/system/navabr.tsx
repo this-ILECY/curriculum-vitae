889d7abf007a8b7a2826c87204a3e7aa14469c30
amirhoseinMardani <this.ilecy@gmail.com>
2024-10-15 21:11:44 +0330

    me.png + navbar fixed + page gradient

--- src/app/components/system/navabr.tsx
+++ src/app/components/system/navabr.tsx
@@ -2,8 +2,9 @@
 
 import { useState } from "react"
 import { HomeIcon, BriefcaseIcon, LightbulbIcon, GraduateIcon, AtSymbolIcon, DownloadIcon } from "../theme/icons"
+import { ComponentProps } from "@/app/interfaces/component.interface";
 
-export default function Navbar() {
+export default function Navbar({ className }: ComponentProps) {
 
     const [activeIndex, setActiveIndex] = useState(0)
 
@@ -12,32 +13,32 @@ export default function Navbar() {
     }
 
     return (
-        <span className="flex flex-col gap-20 w-fit font-hubot font-extralight text-xl">
+        <span className={"flex flex-col gap-20 w-fit font-hubot font-extralight text-xl z-10 " + className}>
             <span className="flex flex-col gap-9">
                 <span onClick={() => activateNavItem(0)} className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer " + (activeIndex === 0 ? "text-primary-300" : "")}>
                     <HomeIcon className="w-9" />
-                    <p>Home</p>
+                    <p className="pr-3">Home</p>
                 </span>
                 <span onClick={() => activateNavItem(1)} className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer " + (activeIndex === 1 ? "text-primary-300" : "")}>
                     <BriefcaseIcon className="w-9" />
-                    <p>Experience</p>
+                    <p className="pr-3">Experience</p>
                 </span>
                 <span onClick={() => activateNavItem(2)} className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer " + (activeIndex === 2 ? "text-primary-300" : "")}>
                     <LightbulbIcon className="w-9" />
-                    <p>Projects</p>
+                    <p className="pr-3">Projects</p>
                 </span>
                 <span onClick={() => activateNavItem(3)} className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer " + (activeIndex === 3 ? "text-primary-300" : "")}>
                     <GraduateIcon className="w-9" />
-                    <p>Skills</p>
+                    <p className="pr-3">Skills</p>
                 </span>
                 <span onClick={() => activateNavItem(4)} className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer " + (activeIndex === 4 ? "text-primary-300" : "")}>
                     <AtSymbolIcon className="w-9" />
-                    <p>Contact</p>
+                    <p className="pr-3">Contact</p>
                 </span>
             </span>
             <span className={"navItem p-1.5 rounded-2xl w-fit simple-transitoin flex flex-row items-center gap-3 cursor-pointer"}>
                 <DownloadIcon className="w-9" />
-                <p>Download CV</p>
+                <p className="pr-3">Download CV</p>
             </span>
         </span>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/system/navabr";
+import me from '../assets/png/me.png'
 
 export default function Home() {
 
 
 
   return (
-    <span>
+    <span className="flex w-full h-full relative items-center content-center p-24">
+      <span className="absolute h-full w-full top-0 right-0 greenlayer"></span>
+      <span className="absolute h-full w-full top-0 right-0 bluelayer"></span>
+      <span className="absolute h-full w-full top-0 right-0 blackhorizontal"></span>
+      <span className="absolute h-full w-full top-0 right-0 blackedge"></span>
+      <span className="absolute h-full w-full top-0 right-0 blackvertical"></span>
+      <img className="h-full absolute right-0 bottom-0" src={me.src} alt="" />
       <Navbar />
       {/* <ChevronDownIcon className="w-10" />
       <CalendarIcon className="w-10" />
