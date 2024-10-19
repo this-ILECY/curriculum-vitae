4da95599948c46d23babb357aa255d4aafa7fac8
amirhoseinMardani <this.ilecy@gmail.com>
2024-10-19 09:51:58 +0330

    scrol detector!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,42 @@
+"use client"
 import Navbar from "./components/system/navabr";
 import me from '../assets/png/me.png'
 import Gradient from "./components/theme/gradient";
-import { ChevronDownIcon } from "./components/theme/icons";
 import Home from "./components/system/home";
+import { useCallback, useEffect, useState } from "react";
 
 export default function HomePage() {
 
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState(null);
 
+  const handleScroll = () => {
+    const currentPosition = window.scrollY;
+
+    if (currentPosition > scrollPosition) {
+      console.log('down');
+
+      setScrollDirection('down')
+    }
+    else {
+      console.log('up');
+
+      setScrollDirection('up')
+    }
+
+    setScrollPosition(currentPosition)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [scrollPosition]);
 
   return (
-    <span className="flex w-full h-full relative items-center content-center p-8p md-py-5p select-none">
+    <span className="flex w-full relative items-center content-center p-8p md-py-5p select-none" style={{ "height": "100.5vh" }}>
       <Gradient />
       <img className="h-full absolute right-0 bottom-0" src={me.src} alt="" />
       <span className="w-full h-fit flex flex-row gap-3">
