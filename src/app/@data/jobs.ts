719dacbcffccc41ff59a244ccd7a57aaa7cbf3ee
amirhoseinMardani <this.ilecy@gmail.com>
2024-11-01 22:25:58 +0330

    better jobs

--- src/app/@data/jobs.ts
+++ src/app/@data/jobs.ts
@@ -1,7 +1,7 @@
 import { ComponentProps } from "react";
 import { RayvarzIcon, WiraIcon } from "../pages/theme/icons"
 
-interface Job {
+export interface Job {
     name: string;
     subtitle: string;
     start: string;
@@ -9,7 +9,7 @@ interface Job {
     description: string;
     type: string,
     diff: string,
-    icon: React.FC<ComponentProps<"svg">>;
+    icon: React.FC<ComponentProps<"svg">>[];
 }
 
 export const Jobs: Job[] = [{
@@ -20,7 +20,7 @@ export const Jobs: Job[] = [{
     type: 'Full-Time',
     diff: '10 mos',
     description: 'Rayvarz, an Iranian software engineering company established in 1368 Hijri khorshidi, works in various domains from Share management to financial systems. They focused on building their new generation of their applications, the fourth ones, on Angular. During my tenure, I began as a front-end developer on the Share app, later shifting to work with Rayvarz`s exclusive framework. It was a dynamic experience, dealing with the evolution of software challenges; Not only keeping existing quality of experience for users but also raising it. Without confusing users with new scenarios, offer’em better experience.',
-    icon: RayvarzIcon
+    icon: [RayvarzIcon]
 },
 {
     name: 'Wira',
@@ -30,5 +30,5 @@ export const Jobs: Job[] = [{
     type: 'Full-Time',
     diff: '1 yrs and 4 mos',
     description: 'During that one year and a quarter at Wira System Pouya, my job was to maintain the main application, WiraPos. This .NET-based product served as the base of our mission to provide complete tool to manage e-commerce manner. At Wira System, we`re committed to simplifying store and warehouse management, improve order processing, and enhancing payment procedures. Through integrations with platforms such as Digikala, Snapp, and 780, we offer advanced e-commerce services. We call our service, WiraPos, the center of our services, serving as a Point of Sale (P.O.S.) system, a warehouse management solution, and an accounting tool.',
-    icon: WiraIcon,
+    icon: [WiraIcon],
 }]
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { useEffect, useRef, useState } from "react"
-import { Jobs } from "../@data/jobs";
+import { Job, Jobs } from "../@data/jobs";
 
 export default function experience() {
+
+    const [activatedJob, setActivatedJob] = useState(0);
 
     const ray = useRef(null);
     const wsp = useRef(null);
@@ -11,86 +13,67 @@ export default function experience() {
 
     const desc = useRef<(HTMLElement | null)[]>([])
 
-    const jobs = Jobs;
-
+    const jobs: Job[] = Jobs;
     useEffect(() => {
-
-        return (() => {
-            activeTab(ray.current, 'right');    
-        })
+        if (activetab.current) activetab.current.style.width = getComputedStyle(activatedJob == 0 ? ray.current : wsp.current).width
+        if (activetab.current) activetab.current.style.height = getComputedStyle(activatedJob == 0 ? ray.current : wsp.current).height
     })
-
-    const activeTab = (ref: HTMLElement, position: 'right' | 'left') => {
+    
+    const activeTab = (ref: HTMLElement, position: number) => {
         if (activetab.current) activetab.current.style.width = getComputedStyle(ref).width
         if (activetab.current) activetab.current.style.height = getComputedStyle(ref).height
 
-        if (position == "right") {
-            if (activetab.current) activetab.current.style.right = 'unset'
-            if (activetab.current) activetab.current.style.left = '0.75rem'
-
-            if (desc.current[1]) desc.current[1].classList.add("hide")
-            setTimeout(() => {
-                if (desc.current[1]) desc.current[1].classList.add('no-height')
-                setTimeout(() => {
-
-                    if (desc.current[0]) desc.current[0].classList.remove(...["hide", 'no-height']);
-                }, 20);
-            }, 500);
+        if (position == 0) {
+            if (activetab.current) activetab.current.classList.add('tab-left')
+            if (activetab.current) activetab.current.classList.remove('tab-right')
+        } else {
+            if (activetab.current) activetab.current.classList.add('tab-right')
+            if (activetab.current) activetab.current.classList.remove('tab-left')
         }
-        else {
-            if (activetab.current) activetab.current.style.left = 'unset'
-            if (activetab.current) activetab.current.style.right = '0.75rem';
-
-            if (desc.current[0]) desc.current[0].classList.add("hide")
-            setTimeout(() => {
-                if (desc.current[0]) desc.current[0].classList.add('no-height')
-                setTimeout(() => {
-                    if (desc.current[1]) desc.current[1].classList.remove(...["hide", 'no-height'])
-                }, 20);
-            }, 500);
-        }
+        setActivatedJob(position)
     }
 
     return (
-        <span className="z-10 text-white grow flex">
+        <span className="experience z-10 text-white grow flex">
             <span className="mx-0p job-box job-box-radius p-4p mx-16 squircle mask-squircle w-full flex flex-col justify-start items-center grow h-full">
                 <span className="relative tabs flex gap-10 py-2 px-3 font-hubot font-light">
-                    <span ref={activetab} className="absolute bg-primary-400 top-2 left-3 h-full z-1 transition-all"></span>
-                    <span ref={ray} onClick={() => { activeTab(ray.current, 'right') }} className="flex justify-center py-1 px-2 z-10 cursor-pointer">Rayvarz</span>
-                    <span ref={wsp} onClick={() => {
-                        activeTab(wsp.current, "left"); console.log(desc);
-                    }} className="flex justify-center py-1 px-2 z-10 cursor-pointer">WSP</span>
+                    <span ref={activetab} className="absolute tab-right bg-primary-400 top-2 left-3 h-full z-1 transition-all tab-left"></span>
+                    <span ref={ray} onClick={() => { activeTab(ray.current, 0) }} className="tab flex justify-center py-1 px-2 z-10 cursor-pointer">Rayvarz</span>
+                    <span ref={wsp} onClick={() => { activeTab(wsp.current, 1) }} className="tab flex justify-center py-1 px-2 z-10 cursor-pointer">WSP</span>
                 </span>
                 <span className="contents">
-                    {jobs.map((job, index) => {
-                        const IconComponent = job.icon
-                        return (
-                            <span ref={(el) => {
-                                desc.current[index] = el; // Assign ref but don't return anything
-                            }} key={index + ''} id={"job-" + index}
-                                className="simple-transition hide no-height flex flex-col gap-10 pt-5 font-hubot">
-                                <span className="header">
-                                    <span className="flex justify-between">
-                                        <span className="flex gap-4p w-full">
-                                            <span className="logo"><IconComponent /></span>
-                                            <span className="flex flex-col justify-between">
-                                                <span className="flex gap-3 items-end">
-                                                    <span className="font-bold text-4xl">{job.name}</span>
-                                                    <span className="font-extralight text-3xl text-2xl-md">{job.subtitle}</span>
-                                                </span>
-                                                <span className="time font-extralight text-base">{job.type} - {job.diff}</span>
-                                            </span>
+                    <span ref={(el) => {
+                        desc.current[activatedJob] = el; // Assign ref but don't return anything
+                    }} key={activatedJob + ''} id={"job-" + activatedJob}
+                        className="jobing simple-transition flex flex-col gap-10 pt-5 font-hubot">
+                        <span className="header">
+                            <span className="top-main flex justify-between">
+                                <span className="flex gap-4p w-full">
+                                    {jobs[activatedJob].icon.map((job, index) => {
+                                        const IconComponent = job;
+                                        return (
+                                            <span key={index} className="logo"><IconComponent className="jobIcon" /></span>
+                                        )
+                                    })}
+                                    <span className="flex flex-col justify-between">
+                                        <span className="jobname flex gap-3 items-end">
+                                            <span className="name font-bold text-4xl">{jobs[activatedJob].name}</span>
+                                            <span className="sub font-extralight text-3xl text-2xl-md">{jobs[activatedJob].subtitle}</span>
                                         </span>
-                                        <span className="flex flex-col text-base italic font-extralight text-no-wrap">
-                                            <span>{job.start}</span>
-                                            <span>{job.end}</span>
+                                        <span>
+                                            <span className="time font-extralight text-base">{jobs[activatedJob].type} - {jobs[activatedJob].diff}</span>
                                         </span>
                                     </span>
                                 </span>
-                                <span className="text-base line-height font-extralight px-6 py-4 bg-NeutralDark-300 rounded-xl">{job.description}</span>
+                                <span className="date flex flex-col text-base italic font-extralight text-no-wrap">
+                                    <span className="spacer"></span>
+                                    <span>{jobs[activatedJob].start}</span>
+                                    <span>{jobs[activatedJob].end}</span>
+                                </span>
                             </span>
-                        )
-                    })}
+                        </span>
+                        <span className="desc text-base line-height font-extralight px-6 py-4 bg-NeutralDark-300 rounded-xl">{jobs[activatedJob].description}</span>
+                    </span>
                 </span>
             </span>
         </span>
