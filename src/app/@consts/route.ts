5530bef9052d06b5a4ca16b599cde3601dc286e7
amirhoseinMardani <this.ilecy@gmail.com>
2024-11-03 14:45:54 +0330

    no const!

--- src/app/@consts/route.ts
+++ src/app/@consts/route.ts
@@ -1,7 +1,7 @@
-export const route = {
-    home: { direction: '/home', id: 0 },
-    experience: { direction: '/experience', id: 1 },
-    projects: { direction: '/projects', id: 2 },
-    skills: { direction: '/skills', id: 3 },
-    contact: { direction: '/contact', id: 4 },
-}
+// export const route = {
+//     home: { direction: '/home', id: 0 },
+//     experience: { direction: '/experience', id: 1 },
+//     projects: { direction: '/projects', id: 2 },
+//     skills: { direction: '/skills', id: 3 },
+//     contact: { direction: '/contact', id: 4 },
+// }
