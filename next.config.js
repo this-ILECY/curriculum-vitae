2700db6d3f60706b02c403faa07e34a45794878a
amirhoseinMardani <this.ilecy@gmail.com>
2024-11-03 14:07:16 +0330

    configure

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 // next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITIORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/home',
-          permanent: true, // Set to true if you want a 308 permanent redirect
-        },
-      ];
-    },
-  };
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Set to true if you want a 308 permanent redirect
+      },
+    ];
+  },
+};
